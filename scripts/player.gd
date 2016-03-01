1ef3c9d6e0a2ec3548b2828a4e246e9394f04b48
Allexit <horde98@gmail.com>
2016-03-02 01:08:05 +0200

    Added movement and animation to character

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,51 @@
 
 extends KinematicBody2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+const RIGHT = Vector2(1, 0)
+const DOWN = Vector2(0, 1)
+
+const RUN_SPEED = 500
+
+var moving = false
+
+onready var anim_player = get_node("anim_player")
+onready var sprite = get_node("sprite")
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	var left = Input.is_action_pressed("move_left")
+	var right = Input.is_action_pressed("move_right")
+	var up = Input.is_action_pressed("move_up")
+	var down = Input.is_action_pressed("move_down")
+	
+	var dir = Vector2(0, 0)
+	if right:
+		dir += RIGHT
+	if left:
+		dir -= RIGHT
+	if down:
+		dir += DOWN
+	if up:
+		dir -= DOWN
+		
+	if dir.length() != 0 and not moving:
+		moving = true
+		_update_moving()
+	elif dir.length() == 0 and moving:
+		moving = false
+		_update_moving()
+		
+	var motion = dir.normalized() * RUN_SPEED
+	
+	move(motion * delta)
+	
+func _update_moving():
+	if moving:
+		anim_player.play("run")
+	else:
+		anim_player.stop()
+		sprite.set_frame(0)
 
 
